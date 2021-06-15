298cb13fa18a5719df952700d64d40f90f2ec05c
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-06-15 18:07:11 +0000

    Add user profile view, url and template

--- EASYJOURNAL/config/urls.py
+++ EASYJOURNAL/config/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
 
+    # other apps paths
     path('', include('home.urls', namespace="home")),
+    path('user/', include('users.urls', namespace="user")),
 ]
