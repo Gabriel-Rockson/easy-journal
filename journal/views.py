46f39870bc3b44ae815567efce1907faea2670f0
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-06-22 23:02:13 +0000

    Add view to create a new journal object

--- journal/views.py
+++ journal/views.py
@@ -1,3 +1,18 @@
+from django.http import request
 from django.shortcuts import render
+from .forms import JournalCreateForm
+from django.views.generic import FormView, CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+class JournalCreateView(LoginRequiredMixin, CreateView):
+    form_class = JournalCreateForm
+    template_name = "journal/create-journal.html"
+    success_url = reverse_lazy("user:user-profile")
+
+    def form_valid(self, form):
+        journal = form.save(commit=False)
+        journal.user = self.request.user
+        journal.save()
+        return super().form_valid(form)
