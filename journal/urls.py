1821ae63479cf623581333d9561d46308951b345
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-06-24 20:36:46 +0000

    Add journal delete url endpoint

--- journal/urls.py
+++ journal/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import JournalCreateView, JournalDetailView, JournalListView, JournalUpdateView
+from .views import JournalCreateView, JournalDeleteView, JournalDetailView, JournalListView, JournalUpdateView
 
 app_name="journal"
 urlpatterns = [
     path("", JournalListView.as_view(), name="list-journals"),
     path("create/", JournalCreateView.as_view(), name="create-journal"),
     path("update/<slug:slug>/", JournalUpdateView.as_view(), name="update-journal"),
+    path("delete/<slug:slug>/", JournalDeleteView.as_view(), name="delete-journal"),
     path("<slug:slug>/", JournalDetailView.as_view(), name="journal-detail"),
 ]
