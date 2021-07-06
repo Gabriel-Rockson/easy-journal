bf8dd162bb072b40c48a931986e70ea634e047eb
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-07-06 12:46:09 +0000

    Add google fonts and register it in the tailwind theme

--- jstoolchain/tailwind.config.js
+++ jstoolchain/tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "varela": ['Varela', "sans-serif"],
+        "monserrat": ['Montserrat', 'sans-serif'],
+        "raleway": ['Raleway', 'sans-serif'],
+      },
+    },
   },
   variants: {
     extend: {},
