73655934ac81bd82ec4ad0988b11e8aa2157a8f7
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-07-06 13:04:05 +0000

    Add custom color palette

--- jstoolchain/tailwind.config.js
+++ jstoolchain/tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
         "monserrat": ['Montserrat', 'sans-serif'],
         "raleway": ['Raleway', 'sans-serif'],
       },
+      colors: {
+        'rich-black-fogra': '#011627',
+        'baby-powder': '#FDFFFC',
+        'tiffany-blue': '#2EC4B6',
+        'rose-madder': '#E71D36',
+        'orange-peel': '#FF9F1C',
+      },
     },
   },
   variants: {
